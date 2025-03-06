cc18b84710a0914df4978a96dcc8ceeb8d819996
okno <paul.delafosse@protonmail.com>
2025-03-06 20:30:21 +0100

    Remove stale feature flag

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,8 @@ use crate::manager::InMemoryFeatureManager;
 mod config;
 mod manager;
 
-pub static FEATURE_X_ENABLED: &str = "feature_x_enabled";
-
 fn main() {
     let result = fs::read_to_string("tto").unwrap();
     let manager = serde_json::from_str::<InMemoryFeatureManager>(&result).unwrap();
     config::feature_x(manager);
 }
-
