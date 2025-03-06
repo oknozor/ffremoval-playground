467dec350191fc35fc275834dcbfff69c810bd63
Paul Delafosse <paul.delafosse@protonmail.com>
2025-03-06 19:52:17 +0100

    chore: split

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
-pub static FEATURE_X_ENABLED: bool = true;  // This is the flag we want to remove later
+use crate::FEATURE_X_ENABLED;
 
 pub fn feature_x() {
     if FEATURE_X_ENABLED {
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod config;
 
+pub static FEATURE_X_ENABLED: bool = true;
+
 fn main() {
     config::feature_x();
 }
