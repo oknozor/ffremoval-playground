14ac1f996aa87b30fa9e22e9aa1da14064bac5bc
okno <paul.delafosse@protonmail.com>
2025-03-06 15:29:43 +0100

    Remove stale feature flag

--- src/config.rs
+++ src/config.rs
@@ -1,9 +1,6 @@
-pub static FEATURE_X_ENABLED: bool = true;  // This is the flag we want to remove later
-
+ ```
 pub fn feature_x() {
-    if FEATURE_X_ENABLED {
-        println!("Feature X is enabled!");
-    } else {
-        println!("Feature X is disabled.");
-    }
+    // The following line was removed as it relates to the feature flag FEATURE_X_ENABLED.
+    // pub static FEATURE_X_ENABLED: bool = true;
 }
+```
