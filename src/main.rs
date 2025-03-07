8f2e845e9e7d6899685350809d8259ab0a491822
oknozor <paul.delafosse@protonmail.com>
2025-03-07 14:24:05 +0100

    chore(ff): Remove feature flag {}

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,5 @@
-use std::fs;
-use crate::manager::InMemoryFeatureManager;
-
-mod config;
-mod manager;
-
-pub static X_ACTIVATED: &str = "feature_x_enabled";
-
 fn main() {
     let result = fs::read_to_string("tto").unwrap();
     let manager = serde_json::from_str::<InMemoryFeatureManager>(&result).unwrap();
     config::do_stuff(manager);
 }
-
