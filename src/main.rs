370f6279df23f415969162601be32d0d07c404f2
oknozor <paul.delafosse@protonmail.com>
2025-03-07 11:30:55 +0100

    chore(ff): Remove feature flag {}

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,8 @@ use crate::manager::InMemoryFeatureManager;
 mod config;
 mod manager;
 
-pub static X_ACTIVATED: &str = "feature_x_enabled";
-
 fn main() {
     let result = fs::read_to_string("tto").unwrap();
     let manager = serde_json::from_str::<InMemoryFeatureManager>(&result).unwrap();
-    config::do_stuff(manager);
+config::do_stuff(manager);
 }
-
